860f570fdbc20a85c1005b37c349d736f70c97cd
AliceShynie <47762521+AliceShynie@users.noreply.github.com>
2020-11-07 14:54:46 +0000

    Future

---
+++ ProgrammingGold.js
@@ -0,0 +1,50 @@
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+}
+
+function openModal1(modal1) {
+    if (modal == null) return
+    modal1.classList.add('active')
+  }
+  
+  function closeModal1(modal1) {
+    if (modal == null) return
+    modal1.classList.remove('active')
+  }
+
+  function openModal2(modal2) {
+    if (modal == null) return
+    modal1.classList.add('active')
+  }
+  
+  function closeModal2(modal2) {
+    if (modal == null) return
+    modal1.classList.remove('active')
+  }
+
+  function openModal3(modal3) {
+    if (modal == null) return
+    modal1.classList.add('active')
+  }
+  
+  function closeModal3(modal3) {
+    if (modal == null) return
+    modal1.classList.remove('active')
+  }
+
+  function openModal4(modal4) {
+    if (modal == null) return
+    modal1.classList.add('active')
+  }
+  
+  function closeModal4(modal4) {
+    if (modal == null) return
+    modal1.classList.remove('active')
+  }
